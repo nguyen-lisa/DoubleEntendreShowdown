514833a65ef8a33df587d8616f49f869e736a089
lisanguyen <leechaschoolstuff@gmail.com>
2023-11-08 02:15:44 -0500

    using ttkbootstrap

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import pandas as pd #data analysis library
+import ttkbootstrap as ttk
+import tkinter
+import pandas as pd
 import random
-import tkinter as tk
+from ttkbootstrap.constants import *
 from tkinter import *
 
-root = tk.Tk()
-root.resizable(False, False)
-root.geometry('600x200')
-root.title('Double Entendre Showdown')
+app = ttk.Window(themename="superhero")
+app.resizable(False, False)
+app.geometry('650x350')
+app.title('Double Entendre Showdown')
 
 df = pd.read_csv("prompts_ff.csv") #reads spreadsheet 
 
@@ -16,29 +18,42 @@ safe_clean = safe.values.tolist()
 danger = df['Danger'].dropna()
 danger_clean = danger.tolist()
 
-#random.choice(safe_clean) + '\n' + random.choice(danger_clean)
+label = ttk.Label(
+    app,
+    text="Double Entendre Showdown"
+)
+label.pack(pady=30)
+label.config(font=("Arial", 20, "bold"))
+
+prompt_frame = ttk.Frame(app)
+prompt_frame.pack(
+    pady=5,
+    padx=5,
+    fill="x"
+)
 
 def setTextInput(text):
-    textExample.configure(state="normal")
+    textExample.configure(state="normal", font=("Comic Sans", 12))
     textExample.delete(1.0, "end")
     textExample.insert(1.0, text)
     textExample.configure(state="disabled")
 
-textExample = tk.Text(
-    root,
-    height=10
+textExample = ttk.Text(
+    app,
+    height=5
 )
+textExample.configure(state="disabled")
 textExample.pack()
 
-button = tk.Button(
-    root,
-    height="5",
-    width="50",
-    #bd="10",
-    text="Generate New Prompt",
-    command=lambda: setTextInput(random.choice(safe_clean) + '\n' + random.choice(danger_clean))
-)
-button.place(x=20, y=150)
-button.pack()
+ttk.Label(prompt_frame, text="Prompt").pack(side="left", padx=5)
+#ttk.Text(prompt_frame).pack(side="left", fill="x", expand=True, padx=5)
 
-root.mainloop()
+button_frame = ttk.Frame(app)
+button_frame.pack(pady=5, padx=5,fill="x")
+ttk.Button(button_frame,
+           text="Generate New Prompt",
+           bootstyle="info",
+           command=lambda: setTextInput(random.choice(safe_clean) + '\n' + random.choice(danger_clean))
+           ).pack(padx=10)
+
+app.mainloop()
---
+++ oldMain.py
@@ -0,0 +1,46 @@
+import pandas as pd #data analysis library
+import random
+import tkinter as tk
+from tkinter import *
+#import ttkbootstrap as ttk
+
+root = tk.Tk()
+root.resizable(False, False)
+root.geometry('600x200')
+root.title('Double Entendre Showdown')
+#root.theme_use('aqua')
+
+df = pd.read_csv("prompts_ff.csv") #reads spreadsheet 
+
+safe = df['Safe'].dropna()
+safe_clean = safe.values.tolist()
+
+danger = df['Danger'].dropna()
+danger_clean = danger.tolist()
+
+#random.choice(safe_clean) + '\n' + random.choice(danger_clean)
+
+def setTextInput(text):
+    textExample.configure(state="normal")
+    textExample.delete(1.0, "end")
+    textExample.insert(1.0, text)
+    textExample.configure(state="disabled")
+
+textExample = tk.Text(
+    root,
+    height=10
+)
+textExample.pack()
+
+button = tk.Button(
+    root,
+    height="5",
+    width="50",
+    #bd="10",
+    text="Generate New Prompt",
+    command=lambda: setTextInput(random.choice(safe_clean) + '\n' + random.choice(danger_clean))
+)
+button.place(x=20, y=150)
+button.pack()
+
+root.mainloop()
